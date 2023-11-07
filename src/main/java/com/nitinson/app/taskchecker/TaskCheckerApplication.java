303d4f05c19bc1d265235d3ccf033a2c1aacf413
Nitin Soni <nitin.soni@ista.com>
2023-11-07 19:46:01 +0100

    Initial template

---
+++ src/main/java/com/nitinson/app/taskchecker/TaskCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.nitinson.app.taskchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCheckerApplication.class, args);
+	}
+
+}
