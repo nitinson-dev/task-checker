d96b5839a3b615e288cf5885050ddeb76dd00208
Nitin Soni <nitinson.dev@gmail.com>
2023-11-24 23:47:59 +0100

    Added test support dependencies

--- src/test/java/com/nitinson/app/taskchecker/TaskCheckerApplicationTests.java
+++ src/test/java/com/nitinson/app/taskchecker/TaskCheckerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.nitinson.app.taskchecker;
 
+import com.nitinson.app.taskchecker.rest.TaskController;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class TaskCheckerApplicationTests {
+public class TaskCheckerApplicationTests {
+
+	@Autowired
+	private TaskController taskController;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertThat(taskController).isNotNull();
 	}
 
 }
